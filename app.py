bcc8c9439433f444fc4e4f68ec78491a1185e9fa
Yesica <53982363+yesicaochoa@users.noreply.github.com>
2025-06-11 10:44:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -15,10 +16,5 @@ def index():
     return render_template('index.html', emocion=emocion)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-import os
-
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
